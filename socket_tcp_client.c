0532e626a15284567e551dc588393ed558bad4fe
renqiyang <renqiyang@bytedance.com>
2021-01-20 11:33:24 +0800

    update

--- socket_tcp_client.c
+++ socket_tcp_client.c
@@ -47,7 +47,7 @@ int main() {
     servaddr.sin_family = AF_INET;
     servaddr.sin_addr.s_addr=inet_addr("10.227.8.96");//IP地址设置成INADDR_ANY,让系统自动获取本机的IP地址
     servaddr.sin_port=htons(DEFAULT_PORT);
-    int connectRes
+    int connectRes;
     connectRes = connect(socket_fd, (struct sockaddr*)&servaddr, sizeof(servaddr));
     printf("connect res=%d",connectRes);
     if( connectRes< 0)
