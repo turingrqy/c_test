02794de1e74c3c80233b331dd00e7b4df3422b71
renqiyang <renqiyang@bytedance.com>
2021-02-03 16:20:12 +0800

    update

---
+++ pc.c
@@ -0,0 +1,29 @@
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/mman.h>       //for mmap
+#include <stdint.h>         //for uint32_t
+#include <errno.h>
+#include <linux/futex.h>    //for FUTEX_WAIT FUTEX_WAKE
+#include <sys/time.h>       //for struct timespec
+#include <sys/syscall.h>    //for SYS_futex
+
+int main() {
+    pid_t pid;
+    char buf[10] = {'a'}
+    char **a = &buf;
+    pid = fork();
+    if (pid == 0) {
+        *a = "bbbbbbbbbb"
+        printf("child str=%s\n",*a);
+        printf("child 指针（地址）的值为：OX%p\n",*a);
+    } else {
+        wait(null);
+        printf("main str=%s\n",*a);
+        printf("child 指针（地址）的值为：OX%p\n",*a);
+    }
+}
